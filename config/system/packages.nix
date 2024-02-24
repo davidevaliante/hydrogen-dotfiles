d2ad0f8e98826d6ae4a728d29e61b61a22296121
Davide Valiante <dav.valiante@gmail.com>
2024-02-24 21:46:41 +0100

    added bottom and removed btop

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -6,7 +6,7 @@
 
   # List System Programs
   environment.systemPackages = with pkgs; [
-    wget curl git cmatrix lolcat neofetch htop btop libvirt
+    wget curl git cmatrix lolcat neofetch htop bottom libvirt
     polkit_gnome lm_sensors unzip unrar libnotify eza
     v4l-utils ydotool wl-clipboard socat cowsay lsd lshw
     pkg-config meson hugo gnumake ninja go nodejs symbola
