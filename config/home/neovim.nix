8a7b641f13326841ff213d0a2e8f5635a09ca660
Davide Valiante <dav.valiante@gmail.com>
2024-03-08 09:01:33 +0100

    cmp

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -164,25 +164,27 @@ in {
             modes = [ "i" "s" ];
           };
         };
-        window = {
-          completion = {
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
-            col_offset = -3;
-            side_padding = 0;
+        settings = {
+          window = {
+            completion = {
+              winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
+              col_offset = -3;
+              side_padding = 0;
+            };
           };
-        };
-        formatting = {
-          fields = [ "kind" "abbrv" "menu" ];
-          format = ''
-            function(entry, vim_item)
-              local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-              local strings = vim.split(kind.kind, "%s", { trimempty = true })
-              kind.kind = " " .. (strings[1] or "") .. " "
-              kind.menu = "    (" .. (strings[2] or "") .. ")"
+          formatting = {
+            fields = [ "kind" "abbrv" "menu" ];
+            format = ''
+              function(entry, vim_item)
+                local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+                local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                kind.kind = " " .. (strings[1] or "") .. " "
+                kind.menu = "    (" .. (strings[2] or "") .. ")"
 
-              return kind
-            end
-          '';
+                return kind
+              end
+            '';
+          };
         };
       };
 
